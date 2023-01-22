03ad7e63142756972ec530893c282418332b82e3
sks006 <shimkhan07@gmail.com>
2023-01-22 08:20:48 +0600

    I give some info in app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
 
-    console.log(`Server is runing on port ${process.env.PORT}`)
+        console.log(`Server is runing on port ${process.env.PORT}`)
 
-})
+    })
+    /* $env:PORT=5000
+     npm start   */
